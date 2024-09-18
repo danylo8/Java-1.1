751948faddc94846d507a3975c5295f1eb50eb84
danylo8 <ozai.avatar@gmail.com>
2024-09-18 23:25:51 +0000

    Pending changes exported from your codespace

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello. My name is Danylo");
+        System.out.print("My favorite color is blue.");
+        System.out.println(" I like playing videogames and I enjoy swimming.");   
+        System.out.println("");    
+        System.out.print("How about you?");
+    }
+
+}
