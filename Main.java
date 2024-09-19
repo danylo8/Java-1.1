7082835d0fd10f5e7bc11a9877f10c3d50ea9a40
danylo8 <ozai.avatar@gmail.com>
2024-09-18 20:36:03 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,12 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello. My name is Danylo");
+        System.out.println("Hello. My name is Danylo.");
+        System.out.print("");
         System.out.print("My favorite color is blue.");
-        System.out.println(" I like playing videogames and I enjoy swimming.");   
+        String age=" I am 16 years old.";
+        System.out.println(age);
+        System.out.println("I like playing videogames and I enjoy swimming.");   
         System.out.println("");    
         System.out.print("How about you?");
     }
